d9658c2e1f0fe638235f4bd3426ea35c58769eee
LeahKagure <leah.kagure98@gmail.com>
2022-08-06 03:10:34 -0700

    User

--- models/user.py
+++ models/user.py
@@ -12,14 +12,7 @@ class User(BaseModel):
         first_name (str): the first name of the user
         last_name (str): the last name of the user
     """
-    def __init__(self, email="", password="",
-                 first_name="", last_name="", *args, **kwargs):
-        super(User, self).__init__(*args, **kwargs)
-        self.email = email
-        self.password = password
-        self.first_name = first_name
-        self.last_name = last_name
-
-    def __str__(self):
-        return f'User({self.email}, {self.password},\
-            {self.first_name}, {self.last_name})'
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
